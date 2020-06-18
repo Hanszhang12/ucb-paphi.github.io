d207472ee81cec30d488d42502587fbe7edefc4d
John Doe <hanszhang2022bear@berkeley.edu>
2020-06-18 02:29:40 -0700

    brothers page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -104,17 +104,16 @@ let menuIcon = document.querySelector('.menuIcon');
 
 
       // firebase
+
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
           // User is signed in.
-
           document.getElementById("user_div").style.display = "block";
           document.getElementById("login_div").style.display = "none";
 
           var user = firebase.auth().currentUser;
 
           if(user != null){
-
             var email_id = user.email;
             document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
           }
@@ -135,7 +134,6 @@ let menuIcon = document.querySelector('.menuIcon');
           // Handle Errors here.
           var errorCode = error.code;
           var errorMessage = error.message;
-
           window.alert("Error : " + errorMessage);
 
           // ...
